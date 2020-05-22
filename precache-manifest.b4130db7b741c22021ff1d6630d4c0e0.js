f5117f32783ee6ae5297b8617a4844bc2b2d1c6d
DazaiLux <proyectpag@gmail.com>
2020-05-22 00:01:55 -0500

    Updates

---
+++ precache-manifest.b4130db7b741c22021ff1d6630d4c0e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "429850aae9720900aa728dd28c5555bf",
+    "url": "/Social-Media-Dashboard/index.html"
+  },
+  {
+    "revision": "ae2cee4d2d0b265a1b7f",
+    "url": "/Social-Media-Dashboard/static/css/main.505ead44.chunk.css"
+  },
+  {
+    "revision": "aef2a28a6cef82c24473",
+    "url": "/Social-Media-Dashboard/static/js/2.98824f87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Social-Media-Dashboard/static/js/2.98824f87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae2cee4d2d0b265a1b7f",
+    "url": "/Social-Media-Dashboard/static/js/main.c94a5b4c.chunk.js"
+  },
+  {
+    "revision": "9016c5830db9d5bf7b92",
+    "url": "/Social-Media-Dashboard/static/js/runtime-main.73a9711a.js"
+  }
+]);
